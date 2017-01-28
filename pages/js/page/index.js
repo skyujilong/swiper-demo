1acfd3ed5f475ee9f208392d1709d4664df19936
YJL <skyujilong@163.com>
2017-01-28 18:00:04 +0800

    add event support

--- pages/js/page/index.js
+++ pages/js/page/index.js
@@ -4,10 +4,24 @@ require('SwiperCss');
 require('../../scss/index.scss');
 // console.log($);
 var Swiper = require('Swiper');
-var swiper = new Swiper('.swiper-container', {
+var swiper1 = new Swiper('#swiper1', {
     scrollbar: '.swiper-scrollbar',
     direction: 'vertical',
     slidesPerView: 'auto',
     mousewheelControl: true,
     freeMode: true
+});
+
+var swiper2 = new Swiper('#swiper2', {
+    scrollbar: '.swiper-scrollbar',
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    freeMode: true,
+    onTap: function(swiper,event){
+        //TODO 通过绑定的 tap事件进行分发点击事件。
+        //event.target 然后判断对应的className之类的方式
+        console.log(swiper);
+        console.log(event);
+    }
 });
